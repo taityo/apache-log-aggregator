6a7f02e2168c8d247e70cfcbdab5c25008e1ddc9
taicho <taicho.nv@gmail.com>
2020-07-10 11:26:45 +0000

    edit readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# fixpoint-coding-test
+# Apache Log Aggregator
+
+これはApacheのアクセスログを集計するCLIです。
+
+## Installation
+### pipenv 
+```
+git clone https://github.com/taityo/fixpoint-coding-test.git
+
+pipenv install
+```
+
+### pip
+```
+git clone https://github.com/taityo/fixpoint-coding-test.git
+
+pip install -r requirements.txt
+```
+
+## Usage
+```
+python agg.py [time/host] [-f FILE] [-t TERM]
+```
+
+## Example
+時間ごとにアクセス件数を集計する
+```
+python agg.py time -f log/access_log
+```
+
+リモートホストごとにアクセス件数を集計する
+```
+python agg.py host -f log/access_log
+```
+
+複数ファイルを集計する場合は、コンマ(,)で区切る
+```
+python agg.py time -f log/access_log,log/access_log2
+```
+
+期間を指定して集計する
+```
+python agg.py time -f log/access_log -t 2020/03/03:2020/05/02
+```
--- agg.py
+++ agg.py
@@ -8,27 +8,26 @@ if __name__ == "__main__":
     
     # コマンドを定義
     parser = ap.ArgumentParser(description="Webサーバのアクセスログを集計するCLI")
-    parser.add_argument("agg", help="集計方法をしている。timeかhostを指定する事ができる")
+    parser.add_argument("method", help="集計方法を指定する。timeかhostを指定する事ができる")
     parser.add_argument("-f", "--file", type=lambda x:x.split(","), help="集計するファイルを指定する")
     parser.add_argument("-t", "--term", type=lambda x:x.split(":"), help="集計した期間を指定する")
     
     # 引数の解析と読み込み
     args = parser.parse_args()
-    agg = args.agg
+    method = args.method
     filenames = args.file
     term = args.term
     
     # 集計オブジェクトを作成
-    if agg == "time":
+    if method == "time":
         aggregator = TimeAccessAggregator()
-    elif agg == "host":
+    elif method == "host":
         aggregator = RemoteHostAccessAggregator()
     else:
         exit()
 
     # ファイルを読み込み、集計
     for fn in filenames:
-        # ファイルリーダーを作成
         reader = pd.read_table(fn, chunksize=50)
 
         for lines in reader:
@@ -48,9 +47,9 @@ if __name__ == "__main__":
     
     # 集計結果を出力
     result = aggregator.get_result()
-    if args.agg == "time":
+    if method == "time":
         print("Time      \tAccessCount")
-    elif args.agg == "host":
+    elif method == "host":
         print("RemoteHost\tAccessCount")
     for key in result.keys():
         print(f"{key}\t{result[key]}")
